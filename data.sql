dcdd3effd1618f8ea4db043630c375c4c439c4ec
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-10-08 20:00:13 -0700

    chore: add data for seed
    Also reorganized "drop table" order, because messages references users,
    so messages has to be dropped first.

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
     username text PRIMARY KEY,
@@ -18,4 +18,43 @@ CREATE TABLE messages (
     body text NOT NULL,
     sent_at timestamp with time zone NOT NULL,
     read_at timestamp with time zone
+);
+
+INSERT INTO users (username, password, first_name, last_name, phone, join_at, last_login_at)
+VALUES
+(
+    'user1',
+    '$2b$12$dqLa9.t5gvA1DIcw1gd4helJY0AAoE6ur2TnlDxY.9Z4N8ZKiSPOq',
+    'first',
+    'last',
+    '123-456-7890',
+    current_timestamp,
+    null
+),
+(
+    'user2',
+    '$2b$12$e.6kcFWOuxU40hxOKx6gO.SwYR4n4o0aI31Mkjql0ARDKOfo5f52C',
+    'first',
+    'last',
+    '123-456-7890',
+    current_timestamp,
+    null
+),
+(
+    'user3',
+    '$2b$12$2CgwCSjykDE7QMCvbxiGrutlVUwguqlc9T4U021Mkm/IilGerrV7S',
+    'first',
+    'last',
+    '123-456-7890',
+    current_timestamp,
+    null
+);
+
+INSERT INTO messages (from_username, to_username, body, sent_at, read_at)
+VALUES (
+    'user1',
+    'user2',
+    'message body',
+    current_timestamp,
+    null
 );
